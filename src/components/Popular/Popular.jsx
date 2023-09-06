406f0bb1bd0a1d582c1dcd26a740c3f0066d3e5f
waleed2000× <waleedahmed2000x@gmail.com>
2023-09-06 12:47:55 +0500

    Popular Slider

--- src/components/Popular/Popular.jsx
+++ src/components/Popular/Popular.jsx
@@ -1,9 +1,11 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
-import Marquee from "react-fast-marquee";
 
 export default function Popular() {
   const [popularMovies, setPopularMovies] = useState([]);
+  const [isHovered, setIsHovered] = useState(false);
+  const [hoveredImage, setHoveredImage] = useState(null);
+
   useEffect(() => {
     const options = {
       method: "GET",
@@ -16,32 +18,49 @@ export default function Popular() {
     axios
       .request(options)
       .then(function (response) {
+        console.log(response.data.results);
         setPopularMovies(response.data.results);
       })
       .catch(function (error) {
         console.error(error);
       });
   }, []);
+  const handleImageHover = (title) => {
+    setIsHovered(true);
+    setHoveredImage(title);
+  };
 
-  console.log(popularMovies);
+  const handleImageLeave = () => {
+    setIsHovered(false);
+    setHoveredImage(null);
+  };
+
   return (
-    <div className="popularParent">
-      <div className="popularMainImage">
-        {popularMovies.map((movie, i) => {
-          return (
-            <div key={i}>
-              <Marquee>
+    <div className="mainParent">
+      <div className="popularParent">
+        <div className="popularMainImage">
+          {popularMovies.map((movie, i) => {
+            return (
+              <div
+                key={i}
+                className="imageContainer"
+                onMouseEnter={() => handleImageHover(movie.original_title)}
+                onMouseLeave={handleImageLeave}
+              >
                 <div>
+                  <p className={`imageTitle ${isHovered ? "showTitle" : ""}`}>
+                    {hoveredImage}
+                  </p>
                   <img
                     src={`https://image.tmdb.org/t/p/w500/${movie.backdrop_path}`}
+                    alt={movie.original_title}
                   />
                 </div>
-              </Marquee>
-            </div>
-          );
-        })}
+              </div>
+            );
+          })}
+        </div>
       </div>
-      <p>Popular</p>
     </div>
   );
 }
