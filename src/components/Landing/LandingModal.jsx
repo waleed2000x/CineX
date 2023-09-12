384af6b58879f5bfc5e9d33db6da9d2ab4b65573
waleed2000× <waleedahmed2000x@gmail.com>
2023-09-12 23:27:28 +0500

    ~!~

--- src/components/Landing/LandingModal.jsx
+++ src/components/Landing/LandingModal.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/prop-types */
 import { Dialog, DialogContent, Typography } from "@mui/material";
 import { useContext, useEffect } from "react";
 import { ThemeContext } from "../themeContext/ThemeContext";
