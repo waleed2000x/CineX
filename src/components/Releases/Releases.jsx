2c95c0ec13df7cabc9b7bc95622fd374ad3754b6
waleed2000× <waleedahmed2000x@gmail.com>
2023-09-15 19:38:52 +0500

    Releases page

--- src/components/Releases/Releases.jsx
+++ src/components/Releases/Releases.jsx
@@ -1,7 +1,33 @@
+import axios from "axios";
 import FirstSightVideo from "../Popular/FirstSightVideo";
 import AfterEverything from "../media/AFTEREVERYTHING.mp4";
 import AfterEverythingImg from "../media/After-Everything.jpg";
+import { useEffect, useState } from "react";
+
 export default function Releases() {
+  const [releases, setReleases] = useState();
+  useEffect(() => {
+    const options = {
+      method: "GET",
+      url: "https://api.themoviedb.org/3/trending/all/day",
+      params: { language: "en-US" },
+      headers: {
+        accept: "application/json",
+        Authorization:
+          "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI1NzMxODBhZGQ1NTg3NmNkZDE4OTExYTY1MzE1ZjFiMyIsInN1YiI6IjY0ZjZjZmQ4NWYyYjhkMDBhYmNiZDcxNyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.Fb9Iw-2JaPM8_9Wk1FPiYcRgsQqlL3ak1v12FimF7TA",
+      },
+    };
+
+    axios
+      .request(options)
+      .then(function (response) {
+        setReleases(response.data.results);
+        console.log(response.data);
+      })
+      .catch(function (error) {
+        console.error(error);
+      });
+  });
   return (
     <div className="mainParentPopular">
       <FirstSightVideo
