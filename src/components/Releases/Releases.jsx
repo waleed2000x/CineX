771124cfb01e55dc0e2a953376c27bad13faad5d
waleed2000× <waleedahmed2000x@gmail.com>
2023-09-15 19:48:39 +0500

    Releases to completion

--- src/components/Releases/Releases.jsx
+++ src/components/Releases/Releases.jsx
@@ -1,33 +1,32 @@
-import axios from "axios";
+import InfoOutlinedIcon from "@mui/icons-material/InfoOutlined";
 import FirstSightVideo from "../Popular/FirstSightVideo";
 import AfterEverything from "../media/AFTEREVERYTHING.mp4";
 import AfterEverythingImg from "../media/After-Everything.jpg";
-import { useEffect, useState } from "react";
-
+import { useContext, useState } from "react";
+import { ThemeContext } from "@emotion/react";
+import { Button, IconButton, Typography } from "@mui/material";
+import useFetchAPI from "../useFetch/UseFetchAPI";
+import PopularSkeleton from "../Popular/PopularSkeleton";
+import { motion } from "framer-motion";
 export default function Releases() {
-  const [releases, setReleases] = useState();
-  useEffect(() => {
-    const options = {
-      method: "GET",
-      url: "https://api.themoviedb.org/3/trending/all/day",
-      params: { language: "en-US" },
-      headers: {
-        accept: "application/json",
-        Authorization:
-          "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI1NzMxODBhZGQ1NTg3NmNkZDE4OTExYTY1MzE1ZjFiMyIsInN1YiI6IjY0ZjZjZmQ4NWYyYjhkMDBhYmNiZDcxNyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.Fb9Iw-2JaPM8_9Wk1FPiYcRgsQqlL3ak1v12FimF7TA",
-      },
-    };
-
-    axios
-      .request(options)
-      .then(function (response) {
-        setReleases(response.data.results);
-        console.log(response.data);
-      })
-      .catch(function (error) {
-        console.error(error);
-      });
-  });
+  const [showModal, setShowModal] = useState(false);
+  const [selectedMovie, setSelectedMovie] = useState(null);
+  const { theme } = useContext(ThemeContext);
+  const {
+    data: popularMovies,
+    isLoading,
+    error,
+  } = useFetchAPI(
+    "https://api.themoviedb.org/3/movie/upcoming?language=en-US&page=2",
+    {
+      api_key: "573180add55876cdd18911a65315f1b3",
+    }
+  );
+  // Function to handle opening the modal with the selected movie
+  const openModalWithMovie = (movie) => {
+    setSelectedMovie(movie);
+    setShowModal(true);
+  };
   return (
     <div className="mainParentPopular">
       <FirstSightVideo
@@ -39,6 +38,86 @@ export default function Releases() {
           "The fifth and final installment of the AFTER franchise finds Hardin struggling to move forward. Besieged by writer's block and the crushing breakup with Tessa, Hardin travels to Portugal in search of a woman he wronged in the past - and to find himself."
         }
       />
+      <div className="popularParent">
+        <div className="PopularTitle">
+          <Typography
+            variant="h5"
+            fontFamily={"inherit"}
+            fontWeight={"600"}
+            fontSize={"30px"}
+            color={`${theme === "light" ? "black" : "white"}`}
+          >
+            Explore the Latest Blockbusters and Fan Favorites in Our Popular
+            Movies Collection
+          </Typography>
+        </div>
+        <div className="popularMoviesList">
+          {isLoading ? (
+            <PopularSkeleton />
+          ) : error ? (
+            <p>Error: {error.message}</p>
+          ) : popularMovies !== null ? (
+            popularMovies.map((movie, i) => (
+              <motion.div
+                whileHover={{
+                  scale: 1.1,
+                  transition: { duration: 0.5 },
+                }}
+                key={i}
+                className="imageContainer"
+              >
+                <div
+                  className={` imageShader ${
+                    theme === "light" ? "imageShaderLight" : "imageShaderDark"
+                  }`}
+                >
+                  <Button
+                    onClick={() => openModalWithMovie(movie)}
+                    className="watch"
+                    variant="filled"
+                    color="error"
+                    size="small"
+                  >
+                    <Typography variant="p" fontWeight={"800"} color={"white"}>
+                      Watch Now
+                    </Typography>
+                  </Button>
+                  <IconButton
+                    className="infoButton"
+                    variant="filled"
+                    size="small"
+                  >
+                    <InfoOutlinedIcon />
+                  </IconButton>
+                  <div className="movieImageTitle">
+                    <Typography
+                      variant="p"
+                      color={`${theme === "light" ? "black" : "white"}`}
+                    >
+                      {movie.original_title}
+                    </Typography>
+                  </div>
+                </div>
+                <img
+                  src={`https://image.tmdb.org/t/p/w500/${movie.backdrop_path}`}
+                />
+              </motion.div>
+            ))
+          ) : (
+            <p>No data available</p>
+          )}
+        </div>
+      </div>
+      {selectedMovie && (
+        <PopularModal
+          open={showModal}
+          data={selectedMovie}
+          onClose={() => {
+            setShowModal(false);
+            setSelectedMovie(null);
+          }}
+        />
+      )}
     </div>
   );
 }
